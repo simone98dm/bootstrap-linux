6f234b4ca3e6094528f1863079fb1e7a995e2fe7
Simone Dal Mas <simone.dalmas@outlook.it>
2019-08-27 20:30:09 +0200

    Add script to install the vscode extensions

---
+++ vscode-extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/simone98dm/bootstrap-linux/master/extensions.txt
+cat ./extensions.txt | while read line
+do
+   code --install-extension $line
+done
