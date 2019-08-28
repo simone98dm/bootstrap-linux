35b6d75f2a5020134a70ff77d52bb3e906097c6a
Simone Dal Mas <simone.dalmas@outlook.it>
2019-08-28 19:36:42 +0200

    Fix url path

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/simone98dm/bootstrap-linux/master/settings.json .
-wget https://raw.githubusercontent.com/simone98dm/bootstrap-linux/master/extensions.txt .
+wget https://raw.githubusercontent.com/simone98dm/bootstrap-linux/settings-extensions/settings.json .
+wget https://raw.githubusercontent.com/simone98dm/bootstrap-linux/settings-extensions/extensions.txt .
 
 cat ./extensions.txt | while read line
 do
