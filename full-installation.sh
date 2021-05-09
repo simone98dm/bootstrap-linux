b6bfc31d02423ffabe1fd55aa40f7c24332b0f26
Simone <simone.dalmas@outlook.it>
2021-05-09 16:34:09 +0200

    Update full-installation.sh

--- full-installation.sh
+++ full-installation.sh
@@ -1,31 +1,38 @@
-#!/bin/bash
+!/bin/bash
 RED=`tput bold && tput setaf 1`
 GREEN=`tput bold && tput setaf 2`
 YELLOW=`tput bold && tput setaf 3`
 BLUE=`tput bold && tput setaf 4`
 NC=`tput sgr0`
+
 function RED(){
 	echo -e "${RED}[-] ${1}${NC}"
 }
+
 function GREEN(){
 	echo -e "${GREEN}[+] ${1}${NC}"
 }
+
 function YELLOW(){
 	echo -e "${YELLOW}[!] ${1}${NC}"
 }
+
 function BLUE(){
 	echo -e "${BLUE}[*] ${1}${NC}"
 }
+
 if [ $UID -ne 0 ]
 then
 	RED "You must run this script as root!" && echo
 	exit
 fi
+
 BLUE "Running update and upgrade"
 sudo apt-get update > /dev/null;
 sudo apt-get upgrade -y > /dev/null;
 GREEN "Installign apt-transport-https ca-certificates curl gnupg lsb-release wget"
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release wget > /dev/null;
+
 YELLOW "Downloading dependency..."
 GREEN "Downloading chrome"
 wget -O latest-chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
@@ -36,11 +43,13 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 GREEN "Downloading dotnet-core"
 wget -O packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb 
+
 YELLOW "Adding APT source.lst entries"
 GREEN "Adding docker dep"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88 -y
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y > /dev/null
+
 YELLOW "Installing APT packages"
 GREEN "Installing chrome packages"
 sudo dpkg -i latest-chrome.deb > /dev/null;
@@ -74,6 +83,20 @@ GREEN "Installing ffmpeg"
 sudo apt-get install -y ffmpeg > /dev/null;
 GREEN "Installing cmake"
 sudo apt-get install -y cmake > /dev/null;
+GREEN "Installing net-tools"
+sudo apt-get install -y net-tools > /dev/null;
+GREEN "Installing jq"
+sudo apt-get install -y jq > /dev/null;
+GREEN "Installing xclip"
+sudo apt-get install -y xclip > /dev/null;
+GREEN "Installing locate"
+sudo apt-get install -y locate > /dev/null;
+GREEN "Installing vlc"
+sudo apt-get install -y vlc > /dev/null;
+GREEN "Installing openjdk"
+sudo apt-get install -y openjdk-11-jre openjdk-11-jdk > /dev/null;
+GREEN "Installing gnome-tweak"
+sudo apt-get install -y gnome-tweak-tool > /dev/null;
 YELLOW "Installing SNAP packages"
 GREEN "Installing postman"
 sudo snap install postman > /dev/null;
@@ -94,11 +117,11 @@ sudo npm install -g express-generator > /dev/null;
 GREEN "Installing pm2"
 sudo npm install -g pm2 > /dev/null;
 
-
 YELLOW "Installing ZSH"
 sudo apt install -y zsh
 curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 sh install.sh
 sudo chsh -s $(which zsh)
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth 3
+
 exit
