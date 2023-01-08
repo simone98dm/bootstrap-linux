a9087a8c549e5f21f090eb9ad6cb17b747aa8f2a
Simone <simone.dalmas@outlook.it>
2023-01-08 15:12:05 +0100

    Create brew-instal.sh

---
+++ brew-instal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+brew install --cask discord
+brew install --cask burp-suite
+brew install --cask iterm2
+brew install --cask sublime-text
+brew install --cask vlc
+brew install --cask brave-browser
+
+brew install act
+brew install gource
+brew install ffmpeg
+brew install imagemagick
+brew install htop
+brew install micro
+brew install youtube-dl
+brew install zsh
